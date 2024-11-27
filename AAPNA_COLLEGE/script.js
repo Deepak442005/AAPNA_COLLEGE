606d8069ead0ad78c3df121a1ad64caa110c0ca1
ƁᏞᎪᏟᏦ <ddeepakkumar847105@gmail.com>
2024-11-27 19:15:12 +0530

    Add files via upload

---
+++ AAPNA_COLLEGE/script.js
@@ -0,0 +1,5 @@
+// Add an event listener to the "Learn More" button
+const learnMoreButton = document.querySelector("#learnMoreBtn");
+learnMoreButton.addEventListener("click", function() {
+    window.location.href = "index2.html"; // Navigate to index2.html
+});
